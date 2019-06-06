d9b21806b51404890dfa253eedd73f9274ea933f
Daniel Candido <danielfelipec18@hotmail.com>
2019-06-06 17:20:51 -0300

    Arquivos Corretos

---
+++ ApiWorkers/ApiWorkers/Controllers/UsuarioController.cs
@@ -0,0 +1,56 @@
+﻿using ApiWorkers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MyJob.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class UsuarioController : ApiController
+    {
+        // GET: api/Usuario
+        public IEnumerable<Usuario> Get()
+        {
+            Usuario usuarios = new Usuario();
+            return usuarios.ListarUsuarios();
+        }
+
+        // GET: api/Usuario/5
+        public Usuario Get(int id)
+        {
+            Usuario usuario = new Usuario();
+            return usuario.ListarUsuarios().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Usuario
+        public List<Usuario> Post(Usuario usuario)
+        {
+            Usuario _usuario = new Usuario();
+
+            _usuario.Inserir(usuario);
+
+            return _usuario.ListarUsuarios();
+
+        }
+
+        // PUT: api/Usuario/5
+        public Usuario Put(int id, [FromBody]Usuario usuario)
+        {
+            Usuario _usuario = new Usuario();
+
+            return _usuario.Atualizar(id, usuario);
+        }
+
+        // DELETE: api/Usuario/5
+        public void Delete(int id)
+        {
+            Usuario _usuario = new Usuario();
+
+            _usuario.Deletar(id);
+        }
+    }
+}
---
+++ ApiWorkers/ApiWorkers/Models/Usuario.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ApiWorkers.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public int NumeroCasa { get; set; }
+        public string Complemento { get; set; }
+        public string Senha { get; set; }
+
+        public List<Usuario> ListarUsuarios()
+        {
+
+            var caminhoDB = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+
+            var json = File.ReadAllText(caminhoDB);
+
+            var listaUsuarios = JsonConvert.DeserializeObject<List<Usuario>>(json);
+
+            return listaUsuarios;
+        }
+
+        public bool ReescreverArquivo(List<Usuario> listaUsuarios)
+        {
+            var caminhoDB = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+
+            var json = JsonConvert.SerializeObject(listaUsuarios, Formatting.Indented);
+            File.WriteAllText(caminhoDB, json);
+
+            return true;
+
+        }
+
+        public Usuario Inserir(Usuario Usuario)
+        {
+            var listaUsuario = this.ListarUsuarios();
+
+            var maxId = listaUsuario.Max(p => p.Id);
+            Usuario.Id = maxId + 1;
+            listaUsuario.Add(Usuario);
+
+            ReescreverArquivo(listaUsuario);
+
+            return Usuario;
+        }
+
+        public Usuario Atualizar(int Id, Usuario Usuario)
+        {
+            var listaUsuario = this.ListarUsuarios();
+
+            var itemIndex = listaUsuario.FindIndex(p => p.Id == Usuario.Id);
+            if (itemIndex >= 0)
+            {
+                Usuario.Id = Id;
+                listaUsuario[itemIndex] = Usuario;
+            }
+            else
+            {
+                return null;
+            }
+
+            ReescreverArquivo(listaUsuario);
+            return Usuario;
+        }
+
+        public bool Deletar(int Id)
+        {
+            var listaUsuarios = this.ListarUsuarios();
+
+            var itemIndex = listaUsuarios.FindIndex(p => p.Id == Id);
+            if (itemIndex >= 0)
+            {
+                listaUsuarios.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+            ReescreverArquivo(listaUsuarios);
+            return true;
+        }
+    }
+}
